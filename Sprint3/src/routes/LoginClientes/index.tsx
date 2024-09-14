0a185148b396af90d5434cd518a56dcb9771872a
joao1015 <moreira.joaopaulo1993@gmail.com>
2024-09-14 13:03:45 -0300

    Commit das alterações antes de atualizar a branch

---
+++ Sprint3/src/routes/LoginClientes/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Cabecalho from "../../Componentes/Cabecalho";
+import Formulario from "../../Componentes/FormularioCadastrocliente";
+import Rodape from "../../Componentes/FormularioCadastrocliente";
+
+
+
+function Logincliente (){
+
+return(
+
+    <div><Cabecalho></Cabecalho>
+    <Formulario></Formulario>
+    <Rodape></Rodape>
+
+
+
+    </div>
+)
+
+
+}
+
+export default Logincliente
